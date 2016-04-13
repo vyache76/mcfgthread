1f702ac69390df1cb2f22ee76ea63aa83eed0939
lhmouse <lh_mouse@126.com>
2016-04-13 16:32:54 +0800

    More README!

--- build_it.sh
+++ build_it.sh
@@ -5,7 +5,7 @@ set -e
 echo Building shared library...
 mkdir -p m4
 autoreconf -if
-CFLAGS='-O3' ./configure --disable-static --enable-shared
+CFLAGS='-O3' LDFLAGS='-Wl,-s' ./configure --disable-static --enable-shared
 make -j4
 
 echo Testing...
