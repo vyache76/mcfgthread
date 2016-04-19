bc0ceea8969c95e375a7939479211dd6652646f4
lhmouse <lh_mouse@126.com>
2016-04-19 18:47:06 +0800

    Disabled assertions in release builds.

--- build_i686.sh
+++ build_i686.sh
@@ -7,7 +7,7 @@ prefix="$(pwd)/release/mingw32"
 echo Building shared library...
 mkdir -p m4
 autoreconf -i
-CFLAGS='-O3' LDFLAGS='-Wl,-s' ./configure --disable-static --enable-shared --build=i686-w64-mingw32 --host=i686-w64-mingw32 --prefix="$prefix"
+CFLAGS='-O3' CPPFLAGS='-DNDEBUG' LDFLAGS='-Wl,-s' ./configure --disable-static --enable-shared --build=i686-w64-mingw32 --host=i686-w64-mingw32 --prefix="$prefix"
 make clean
 make -j4
 
--- build_x86_64.sh
+++ build_x86_64.sh
@@ -7,7 +7,7 @@ prefix="$(pwd)/release/mingw64"
 echo Building shared library...
 mkdir -p m4
 autoreconf -i
-CFLAGS='-O3' LDFLAGS='-Wl,-s' ./configure --disable-static --enable-shared --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --prefix="$prefix"
+CFLAGS='-O3' CPPFLAGS='-DNDEBUG' LDFLAGS='-Wl,-s' ./configure --disable-static --enable-shared --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --prefix="$prefix"
 make clean
 make -j4
 
