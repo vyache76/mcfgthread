5167480ace8e4d95097f7504348864647c266475
lhmouse <lh_mouse@126.com>
2016-04-19 16:09:54 +0800

    Use a dummy library for static linking to the dynamic one.

--- build_i686.sh
+++ build_i686.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+prefix="$(pwd)/release/mingw32"
+
 echo Building shared library...
 mkdir -p m4
 autoreconf -i
-CFLAGS='-O3' LDFLAGS='-Wl,-s' ./configure --disable-static --enable-shared --build=i686-w64-mingw32 --host=i686-w64-mingw32 --prefix="$(pwd)/release/mingw32"
+CFLAGS='-O3' LDFLAGS='-Wl,-s' ./configure --disable-static --enable-shared --build=i686-w64-mingw32 --host=i686-w64-mingw32 --prefix="$prefix"
 make clean
 make -j4
 
@@ -13,3 +15,5 @@ echo Testing...
 (cd test && ./build_test.sh)
 
 make install
+# Due to Windows limitation we can't link this library as a static one.
+(cd "$prefix/lib" && ln -sf libmcfgthread.dll.a libmcfgthread.a)
--- build_x86_64.sh
+++ build_x86_64.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+prefix="$(pwd)/release/mingw64"
+
 echo Building shared library...
 mkdir -p m4
 autoreconf -i
-CFLAGS='-O3' LDFLAGS='-Wl,-s' ./configure --disable-static --enable-shared --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --prefix="$(pwd)/release/mingw64"
+CFLAGS='-O3' LDFLAGS='-Wl,-s' ./configure --disable-static --enable-shared --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --prefix="$prefix"
 make clean
 make -j4
 
@@ -13,3 +15,5 @@ echo Testing...
 (cd test && ./build_test.sh)
 
 make install
+# Due to Windows limitation we can't link this library as a static one.
+(cd "$prefix/lib" && ln -sf libmcfgthread.dll.a libmcfgthread.a)
