2ab212d48aef0dc0f73ae28d4ec0bb02d933c968
lhmouse <lh_mouse@126.com>
2016-04-20 22:58:01 +0800

    Build script updates.

--- build_x86_64.sh
+++ build_x86_64.sh
@@ -5,16 +5,22 @@ set -e
 prefix="$(pwd)/release/mingw64"
 mkdir -p "$prefix"
 
+build="$(pwd)/build-x86_64"
+mkdir -p "$build"
+
 echo Building shared library...
 mkdir -p m4
 autoreconf -i
-CFLAGS='-O3' CPPFLAGS='-DNDEBUG' LDFLAGS='-Wl,-s' ./configure --disable-static --enable-shared --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --prefix="$prefix"
-make clean
-make -j4
+(cd "$build" &&
+  CFLAGS='-O3' CPPFLAGS='-DNDEBUG' LDFLAGS='-Wl,-s' ../configure --disable-static --enable-shared --build=x86_64-w64-mingw32 --host=x86_64-w64-mingw32 --prefix="$prefix" &&
+  make -j4)
 
 echo Testing...
-(cd test && ./build_test.sh)
+(gcc -std=c11 -Wall -Wextra -Wpedantic -Werror test/test.c -Isrc/env -L"$build/lib/.libs" -lmcfgthread -o test.exe &&
+  PATH=$PATH:"$build/lib/.libs" ./test.exe)
 
-make install
+(cd "$build" &&
+  make install)
 # Due to Windows limitation we can't link this library as a static one.
-(cd "$prefix/lib" && ln -sf libmcfgthread.dll.a libmcfgthread.a)
+(cd "$prefix/lib" &&
+  ln -sf libmcfgthread.dll.a libmcfgthread.a)
--- test/build_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-
-gcc -std=c11 -Wall -Wextra -Wpedantic -Werror test.c -I../src/env -L../lib/.libs -lmcfgthread -otest.exe
-PATH=$PATH:"$(pwd)/../lib/.libs" ./test.exe
