db26539d4007113819a9e9078f81d42d8756bb75
lhmouse <lh_mouse@126.com>
2016-04-22 21:38:50 +0800

    Changed workaround for lack of stdint types in the std namespace for C++98.

--- release/mingw32/include/mcfgthread/env/_crtdef.h
+++ release/mingw32/include/mcfgthread/env/_crtdef.h
@@ -35,14 +35,9 @@
 #	include <cstdint>
 #elif defined(__cplusplus)
 #	include <stdint.h>
-namespace std {
-	using ::    int8_t; using ::   uint8_t;
-	using ::   int16_t; using ::  uint16_t;
-	using ::   int32_t; using ::  uint32_t;
-	using ::   int64_t; using ::  uint64_t;
-	using ::  intptr_t; using :: uintptr_t;
-	using ::  intmax_t; using :: uintmax_t;
-}
+// Assume the types required are in the global namespace. This is not only a dirty workaround but also our last hope.
+#	undef _MCFCRT_STD
+#	define _MCFCRT_STD                   ::
 #else
 #	include <stdint.h>
 #endif
