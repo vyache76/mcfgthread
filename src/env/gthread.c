07a8e1f4eee1e3efa67689cd34522982add9b8db
lhmouse <lh_mouse@126.com>
2016-06-06 15:09:08 +0800

    Minor naming issue.

--- src/env/gthread.c
+++ src/env/gthread.c
@@ -31,12 +31,12 @@ intptr_t __MCFCRT_GthreadUnlockCallbackRecursiveMutex(intptr_t context){
 	__gthread_recursive_mutex_t *const recur_mutex = (__gthread_recursive_mutex_t *)context;
 	_MCFCRT_ASSERT(_MCFCRT_GetCurrentThreadId() == __atomic_load_n(&(recur_mutex->__owner), __ATOMIC_RELAXED));
 
-	const size_t __old_count = recur_mutex->__count;
+	const size_t old_count = recur_mutex->__count;
 	recur_mutex->__count = 0;
 	__atomic_store_n(&(recur_mutex->__owner), 0, __ATOMIC_RELAXED);
 
 	__gthread_mutex_unlock(&(recur_mutex->__mutex));
-	return (intptr_t)__old_count;
+	return (intptr_t)old_count;
 }
 void __MCFCRT_GthreadRelockCallbackRecursiveMutex(intptr_t context, intptr_t unlocked){
 	__gthread_recursive_mutex_t *const recur_mutex = (__gthread_recursive_mutex_t *)context;
@@ -44,7 +44,7 @@ void __MCFCRT_GthreadRelockCallbackRecursiveMutex(intptr_t context, intptr_t unl
 	_MCFCRT_ASSERT((size_t)unlocked >= 1);
 	__gthread_mutex_lock(&(recur_mutex->__mutex));
 
-	const uintptr_t __self = _MCFCRT_GetCurrentThreadId();
-	__atomic_store_n(&(recur_mutex->__owner), __self, __ATOMIC_RELAXED);
+	const uintptr_t self = _MCFCRT_GetCurrentThreadId();
+	__atomic_store_n(&(recur_mutex->__owner), self, __ATOMIC_RELAXED);
 	recur_mutex->__count = (size_t)unlocked;
 }
