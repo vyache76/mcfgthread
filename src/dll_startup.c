f06c2483d92a53037c2092897aea8ec4d00002ac
lhmouse <lh_mouse@126.com>
2016-07-30 13:56:24 +0800

    Prettification.

--- src/dll_startup.c
+++ src/dll_startup.c
@@ -11,52 +11,54 @@ __MCFCRT_C_STDCALL
 BOOL __MCFCRT_DllStartup(HINSTANCE hDll, DWORD dwReason, LPVOID pReserved)
 	__asm__("@__MCFCRT_DllStartup");
 
-static bool g_bInitialized = false;
-
-__MCFCRT_C_STDCALL
-BOOL __MCFCRT_DllStartup(HINSTANCE hDll, DWORD dwReason, LPVOID pReserved){
-	(void)hDll;
-	(void)pReserved;
+static bool RealStartup(void *pInstance, unsigned uReason, bool bDynamic){
+	(void)pInstance;
+	(void)bDynamic;
 
 	bool bRet = true;
 
+	switch(uReason){
+	case DLL_PROCESS_ATTACH:
+		bRet = __MCFCRT_ModuleInit();
+		if(!bRet){
+			goto jCleanup_00;
+		}
+		bRet = __MCFCRT_ThreadEnvInit();
+		if(!bRet){
+			goto jCleanup_01;
+		}
+		break;
+
+	case DLL_THREAD_ATTACH:
+		break;
+
+	case DLL_THREAD_DETACH:
+		__MCFCRT_TlsCleanup();
+		break;
+
+	case DLL_PROCESS_DETACH:
+		__MCFCRT_TlsCleanup();
+		__MCFCRT_ThreadEnvUninit();
+	jCleanup_01:
+		__MCFCRT_ModuleUninit();
+	jCleanup_00:
+		break;
+	}
+
+	return bRet;
+}
+
+__MCFCRT_C_STDCALL
+BOOL __MCFCRT_DllStartup(HINSTANCE hDll, DWORD dwReason, LPVOID pReserved){
+	bool bRet;
+
+	void *const pInstance  = (void *)hDll;
+	const unsigned uReason = dwReason;
+	const bool bDynamic    = !pReserved;
+
 	__MCFCRT_SEH_TOP_BEGIN
 	{
-		switch(dwReason){
-		case DLL_PROCESS_ATTACH:
-			if(g_bInitialized){
-				break;
-			}
-			bRet = __MCFCRT_ModuleInit();
-			if(!bRet){
-				goto jCleanup_00;
-			}
-			bRet = __MCFCRT_ThreadEnvInit();
-			if(!bRet){
-				goto jCleanup_01;
-			}
-			g_bInitialized = true;
-			break;
-
-		case DLL_THREAD_ATTACH:
-			break;
-
-		case DLL_THREAD_DETACH:
-			__MCFCRT_TlsCleanup();
-			break;
-
-		case DLL_PROCESS_DETACH:
-			if(!g_bInitialized){
-				break;
-			}
-			g_bInitialized = false;
-			__MCFCRT_TlsCleanup();
-			__MCFCRT_ThreadEnvUninit();
-		jCleanup_01:
-			__MCFCRT_ModuleUninit();
-		jCleanup_00:
-			break;
-		}
+		bRet = RealStartup(pInstance, uReason, bDynamic);
 	}
 	__MCFCRT_SEH_TOP_END
 
