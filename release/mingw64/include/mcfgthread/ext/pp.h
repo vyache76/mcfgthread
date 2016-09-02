5cc1c72bebe2db6b9f9c8d0c4b5d01c0e52199ee
lhmouse <lh_mouse@126.com>
2016-09-02 22:58:25 +0800

    New assert.h.

---
+++ release/mingw64/include/mcfgthread/ext/pp.h
@@ -0,0 +1,23 @@
+// This file is part of MCFCRT.
+// See MCFLicense.txt for licensing information.
+// Copyleft 2013 - 2016, LH_Mouse. All wrongs reserved.
+
+#ifndef __MCFCRT_EXT_PP_H_
+#define __MCFCRT_EXT_PP_H_
+
+#define _MCFCRT_PP_STRINGIFY(...)             #__VA_ARGS__
+
+#define _MCFCRT_PP_WIDEN(__x_)                L ## __x_
+#define _MCFCRT_PP_UTF8(__x_)                 u8 ## __x_
+#define _MCFCRT_PP_UTF16(__x_)                u ## __x_
+#define _MCFCRT_PP_UTF32(__x_)                U ## __x_
+
+#define _MCFCRT_PP_FIRST(__f_, ...)           __f_
+#define _MCFCRT_PP_REST(__f_, ...)            __VA_ARGS__
+
+#define _MCFCRT_PP_CAT2(__x_, __y_)           __x_ ## __y_
+#define _MCFCRT_PP_CAT3(__x_, __y_, __z_)     __x_ ## __y_ ## __z_
+
+#define _MCFCRT_PP_LAZY(__f_, ...)            __f_(__VA_ARGS__)
+
+#endif
