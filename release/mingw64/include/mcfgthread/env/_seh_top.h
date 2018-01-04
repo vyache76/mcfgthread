dcb925cd5becad154b237ad6bc76f694e1904fa1
Liu Hao <lh_mouse@126.com>
2018-01-04 12:44:39 +0800

    Bump up copyright year by one.
    Signed-off-by: Liu Hao <lh_mouse@126.com>

--- release/mingw64/include/mcfgthread/env/_seh_top.h
+++ release/mingw64/include/mcfgthread/env/_seh_top.h
@@ -1,6 +1,6 @@
 // This file is part of MCFCRT.
 // See MCFLicense.txt for licensing information.
-// Copyleft 2013 - 2017, LH_Mouse. All wrongs reserved.
+// Copyleft 2013 - 2018, LH_Mouse. All wrongs reserved.
 
 #ifndef __MCFCRT_ENV_SEH_TOP_H_
 #define __MCFCRT_ENV_SEH_TOP_H_
--- src/env/gthread.c
+++ src/env/gthread.c
@@ -1,6 +1,6 @@
 // This file is part of MCFCRT.
 // See MCFLicense.txt for licensing information.
-// Copyleft 2013 - 2017, LH_Mouse. All wrongs reserved.
+// Copyleft 2013 - 2018, LH_Mouse. All wrongs reserved.
 
 #define __MCFCRT_GTHREAD_INLINE_OR_EXTERN     extern inline
 #include "gthread.h"
