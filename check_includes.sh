e2786b4cbbddfc87b61c62268f5cd5bdd0774cdd
Liu Hao <lh_mouse@126.com>
2018-05-19 00:24:19 +0800

    Update 'check_includes.sh'.
    Signed-off-by: Liu Hao <lh_mouse@126.com>
    (cherry picked from commit 1c3c9e28857ab565e039e162f1b417a38c884a4c)
    Signed-off-by: Liu Hao <lh_mouse@126.com>

--- check_includes.sh
+++ check_includes.sh
@@ -5,10 +5,17 @@ set -e
 function check_one(){
 	_file="$1"
 	_flags="-Wall -Wextra -Werror"
-	_cmd="i686-w64-mingw32-gcc -std=c99 ${_flags} -x c -c -o /dev/null"
+	_cc=""
+	i686-w64-mingw32-gcc -v &>/dev/null && _cc="i686-w64-mingw32-gcc"
+	x86_64-w64-mingw32-gcc -v &>/dev/null && _cc="x86_64-w64-mingw32-gcc"
+	if test -z "${_cc}"; then
+		echo "No mingw-w64 compiler found." >&2
+		exit 2
+	fi
+	_cmd="${_cc} -std=c99 ${_flags} -x c -c -o /dev/null"
 	echo "Checking \`#include\` directives:  ${_cmd}  \"${_file}\""
 	${_cmd}  "${_file}"
-	_cmd="i686-w64-mingw32-gcc -std=c++11 ${_flags} -x c++ -c -o /dev/null"
+	_cmd="${_cc} -std=c++11 ${_flags} -x c++ -c -o /dev/null"
 	echo "Checking \`#include\` directives:  ${_cmd}  \"${_file}\""
 	${_cmd}  "${_file}"
 }
